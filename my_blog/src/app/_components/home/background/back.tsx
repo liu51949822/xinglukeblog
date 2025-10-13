63a38416397ed62014e42d0e71c6040661bcc98b
嘿嘿嘿 <31526224+liu51949822@users.noreply.github.com>
2025-10-13 22:51:38 +0800

    up

---
+++ my_blog/src/app/_components/home/background/back.tsx
@@ -0,0 +1,7 @@
+export const Back = () => {
+  return  (
+  <div>
+   🤝欢迎来到行路客的小站！👋
+  </div>
+  )
+};
--- my_blog/src/app/_components/home/background/index.tsx
+++ my_blog/src/app/_components/home/background/index.tsx
@@ -1,24 +1,7 @@
 import type { FC } from 'react';
-import LiquidEther from './liquid'; 
-
+import { Back } from './back';  
 export const HomeBackground: FC = () =>  (
- <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-  <LiquidEther
-    colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
-    mouseForce={20}
-    cursorSize={100}
-    isViscous={false}
-    viscous={30}
-    iterationsViscous={32}
-    iterationsPoisson={32}
-    resolution={0.5}
-    isBounce={false}
-    autoDemo={true}
-    autoSpeed={0.5}
-    autoIntensity={2.2}
-    takeoverDuration={0.25}
-    autoResumeDelay={3000}
-    autoRampDuration={0.6}
-  />
-</div>
+  <>
+  <Back />
+  </>
 );
