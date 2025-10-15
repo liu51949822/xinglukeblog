da56df354ce432aec4b48ec408c82fc2cfe69020
liurenjie <1240332437@qq.com>
2025-10-15 15:44:50 +0800

    up

--- my_blog/src/app/(pages)/webnav/page.tsx
+++ my_blog/src/app/(pages)/webnav/page.tsx
@@ -1,18 +1,18 @@
 import { WebNav } from "@/app/_components/web_nav/WebNav";
 import { weblinkConfig } from "@/config/web";
-import { WeblinkConfig } from "@/libs/weblink";
+import { WeblinkConfig, WeblinkType, WeblinkTypeLabel } from "@/libs/weblink";
 
 interface DaohangPageProps {
-  links: WeblinkConfig[];
+  links?: WeblinkConfig[];
 }
 
 const DaohangPage = ({ links = weblinkConfig }: DaohangPageProps) => {
   const transformedLinks = links.map(link => ({
-  name: link.shortName,   
-  url: link.url,          
-  description: link.desc,
-  icon: link.logo,
-  type: link.type ?? 0
+    name: link.shortName,
+    url: link.url,
+    description: link.desc,
+    icon: link.logo,
+    type: WeblinkTypeLabel[(link.type ?? WeblinkType.Default) as WeblinkType]
   }));
 
   return <WebNav links={transformedLinks} />;
--- my_blog/src/app/_components/home/cards/welcome.tsx
+++ my_blog/src/app/_components/home/cards/welcome.tsx
@@ -1,12 +1,5 @@
 'use client';
 import type { FC } from 'react';
-
-// import Link from 'next/link';
-
-// import { RainbowButton } from '../../magicui/rainbow-button';
-// import { TextAnimate } from '../../text/animate';
-// import { SparklesText } from '../../text/sparkles';
-// import $styles from './welcome.module.css';
 export interface HomeWelcomeCardType {
     title: string;
     colorTitle?: string;
@@ -14,39 +7,18 @@ export interface HomeWelcomeCardType {
 }
 
 type Props = HomeWelcomeCardType;
+const fullTetx = `🤝 欢迎来到行路客的小站！👋/n
+这里是记录技术思考与生活感悟的角落，每一篇文章都源于真实的探索与体验。/n
+无论是代码的乐趣、旅途的风景，还是生活的点滴，我都希望能与您分享。/n
+感谢您的停留，愿这里能为您带来一点灵感或温暖。`;
 
 export const HomeWelcomeCard: FC<Props> = ({ title, colorTitle, content }) => {
     return (
         <div className="tw-flex tw-h-full tw-w-full tw-flex-col">
             <div className="tw-flex tw-items-center tw-justify-center tw-text-3xl lg:tw-justify-start lg:tw-text-left lg:tw-text-5xl">
-                {/* <div className="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-full tw-bg-[url('/images/home_bg.png')] tw-bg-cover tw-bg-center tw-opacity-10 tw-flex tw-items-center tw-justify-center tw-text-center tw-text-2xl tw-text-gray-500 tw-px-4">
-   🤝 欢迎来到行路客的小站！👋
-   <br/>
-    这里是记录技术思考与生活感悟的角落，每一篇文章都源于真实的探索与体验。
-    <br/>
-    无论是代码的乐趣、旅途的风景，还是生活的点滴，我都希望能与您分享。
-    <br/>
-    感谢您的停留，愿这里能为您带来一点灵感或温暖。
-  </div> */}
-                 {/* <SparklesText
-                    as={
-                        <span>
-                            {colorTitle && <span className={$styles.colorTitle}>{colorTitle}</span>}
-                        </span>
-                    }
-                />
-            </div>
-            <div className="tw-mt-5 tw-flex-auto tw-py-3 tw-font-lxgw tw-text-xl !tw-leading-8 lg:tw-pr-16">
-                <TextAnimate animation="blurInUp" by="line" once delay={0.8}>
-                    {content}
-                </TextAnimate>
-            </div>
-            <div className="tw-flex tw-w-full tw-items-center tw-justify-center tw-py-3 lg:tw-justify-start lg:tw-py-1">
-                <RainbowButton>
-                    <Link href="https://3rcd.com/classroom/" target="_blank">
-                        点此购买课程🤝
-                    </Link>
-                </RainbowButton> */}
+                <div className="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-full tw-bg-[url('/images/home_bg.png')] tw-bg-cover tw-bg-center tw-opacity-10 tw-flex tw-items-center tw-justify-center tw-text-center tw-text-2xl tw-text-gray-500 tw-px-4">
+                {fullTetx}
+                </div>
             </div> 
         </div>
     );
