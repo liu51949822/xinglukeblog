3189c19c22dbaae12123bf61210db01148af8904
嘿嘿嘿 <31526224+liu51949822@users.noreply.github.com>
2025-10-19 22:24:27 +0800

    uo

--- my_blog/src/app/(pages)/layout.tsx
+++ my_blog/src/app/(pages)/layout.tsx
@@ -10,12 +10,9 @@ import Theme from '../_components/theme';
 import $styles from './layout.module.css';
 export const metadata: Metadata = {
     title: '行路客的小站',
-<<<<<<< HEAD
     description:
         '行路客的小站,记录技术文档和分享生活点滴,专注于全栈开发、编程语言、数据库、云计算等技术领域的知识分享与交流。',
-=======
-    description: '行路客的小站,提供一些有用的资源,包括博客,友链,导航,留言等',
->>>>>>> 34ecd8c3c1ec1992ecdde90d83be16c80d3e5458
+
 };
 
 const AppLayout: FC<PropsWithChildren<{ modal: ReactNode }>> = ({ children, modal }) => (
